1e8cf36cbb2c1392201a8d0ea77336d5881b68ad
Obed Gelly Ousmane Tassango <gellytassango@gmail.com>
2026-07-29 05:38:03 +0200

    Ajout synthese vocale gTTS

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, request
 import fitz
 import os
+from gtts import gTTS
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = "uploads"
+AUDIO_FOLDER = "static"
+
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(AUDIO_FOLDER, exist_ok=True)
 
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
+# Extraction du texte du PDF
 def extraire_pdf(chemin):
     texte = ""
 
@@ -23,12 +28,32 @@ def extraire_pdf(chemin):
     return texte
 
 
+# Résumé simple (temporaire avant l'IA)
 def resume_simple(texte):
     phrases = texte.split(".")
-    
+
     resume = ". ".join(phrases[:5])
 
     return resume
+
+
+# Génération audio
+def generer_audio(texte):
+
+    if not texte.strip():
+        return None
+
+    fichier_audio = "static/resume.mp3"
+
+    tts = gTTS(
+        text=texte[:3000],
+        lang="fr"
+    )
+
+    tts.save(fichier_audio)
+
+    return fichier_audio
+
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -36,6 +61,8 @@ def accueil():
 
     texte = ""
     resume = ""
+    audio = None
+
 
     if request.method == "POST":
 
@@ -48,16 +75,23 @@ def accueil():
 
         fichier.save(chemin)
 
+
         texte = extraire_pdf(chemin)
 
         resume = resume_simple(texte)
 
 
+        audio = generer_audio(resume)
+
+
+
     return render_template(
         "index.html",
         texte=texte,
-        resume=resume
+        resume=resume,
+        audio=audio
     )
+
 
 
 if __name__ == "__main__":
